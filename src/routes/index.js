daf4193f51cd5d4470c6b4951296c7e2c5961e67
danmera1980 <danmera1980@gmail.com>
2023-11-25 15:47:37 -0500

    Configure home and vercel config file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const webhook = require('./webhook.js');
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
 router.use('/webhook', webhook);
 
 module.exports = router;
